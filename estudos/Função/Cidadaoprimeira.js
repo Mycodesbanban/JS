919f0e9ccf4762cffdaeb370959d63fe58804d0c
maycon <mayconsrc1117@gmail.com>
2025-01-23 11:28:27 -0300

    update

---
+++ estudos/Função/Cidadaoprimeira.js
@@ -0,0 +1,53 @@
+// Função em JS e Frist-class Object (Citizens), o que significa que podem ser tratadas como qualquer outro valor
+
+
+// Higher-order function , é uma função que pode fazer pelo menos uma das seguites operações:
+// Receber uma ou mais funções como argumentos
+// Retornar uma função como resultado
+
+// função pode ser tratado como sendo um dado , sendo um dado pode passa essa função como parâmetro , e conseguimos retorna uma função como resposta
+
+// Função de forma literal 
+function fun1(){
+
+}
+// Armazenar uma função em variável
+const fun2= function(){
+
+}
+// Amarzenar uma função em um array
+const array = [ function(a , b ){
+    return a+ b 
+
+},
+fun1 , fun2]
+console.log(array[0] ( 2 , 3))
+
+
+// Amazenar uma função em um atributo de objeto
+const obj={}
+obj.falar= function(){
+    return "Opa!"
+}
+console.log(obj.falar())
+
+// passar uma função com um parametro com outra função
+
+function run ( fun){
+    fun ()
+}
+run ( function (){
+    console.log('Executando...')
+})
+
+// Retorna uma função como parametro ou conter uma função
+function soma ( a , b ){
+    return function( c){
+        console.log(a+b+c)
+    }
+}
+soma( 2, 3 )(4)
+// ou
+const cinco= soma( 2, 3)
+cinco(4)
+// temos esses dois tipos para fazer
---
+++ estudos/Função/pararetorn.js
@@ -0,0 +1,62 @@
+// Parametro e Retorno Opcionais , Em Js Os parâmetros são considerados opcionais por padrão , e podem retorna valores opcionias.
+
+// Isso é bastante útil quando desejamos que uma função tenha um comportamento flexivel.
+
+// Parametros Opcionais
+
+// Os parâmetros em JSD são considerados opcionais por padrão.Se você não passar um argumento para uma função , o paramentro correspondente será undefined.
+
+// exemplo 
+function saudacao(nome){
+    if (nome === undefined){
+        return "Ola visitante"
+    }
+    return `Òla , ${nome}`
+}
+console.log(saudacao("Maria"));
+console.log(saudacao());
+
+// Retorno Opcionais
+
+// uma função em JS pode não retonar um valor explicitamente. se você não usar a instrução Return a função retornará undefined por padrão 
+
+// exemplo
+function calcular( a , b ){
+    if ( b === undefined){
+        return a; // retorna apenas a se o b não for fornecido
+    }
+    return a + b // retorna a soma de a e b 
+}
+console.log(calcular(2 , 2))
+console.log(calcular(5))
+
+// Usando Parâmetros Opcionais e Retornos Opcionais Juntos
+// Você pode combinar parâmetros opcionais e retornos opcionais em uma única função para criar um comportamento mais flexíve
+
+function multiplicar(a, b = 1) {
+    return a * b; // Se 'b' não for fornecido, multiplica 'a' por 1
+}
+
+console.log(multiplicar(5, 2)); // 10
+console.log(multiplicar(5)); // 5 (5 * 1)
+
+// vamos fazer testes
+function area ( largura , altura){
+    const area= largura* altura
+    if ( area > 20 ){
+        console.log(`Valor acima do permitido: ${area}.m²`)
+
+    } else{
+        return area
+    }
+}
+console.log(area(2 , 2 ))
+console.log(area(2))
+console.log(area(20, 8 ))
+
+
+
+
+
+// conclusão
+// Em JavaScript, você pode facilmente trabalhar com parâmetros e retornos opcionais. Isso permite que você crie funções mais flexíveis e reutilizáveis, adaptando-se a diferentes situações sem a necessidade de sobrecarregar funções ou criar várias versões da mesma função
