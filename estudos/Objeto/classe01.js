b39a368667b217f0e46d6b41affd9a8962035bf3
maycon <mayconsrc1117@gmail.com>
2025-02-24 20:57:29 -0300

    Up

--- estudos/Objeto/classe01.js
+++ estudos/Objeto/classe01.js
@@ -7,4 +7,25 @@ class lancamento{
     }  // é um método especial usado para inicializar objetos de uma classe , ele é chamando automanticamente quando um objeto é criado a partir dessa class
 
 }
-cal
+class cliclofinaceiro{
+    constructor(mes , ano){
+        this.mes= mes
+        this.ano=ano
+        this.lancamentos=[]
+    }
+    addlancamento(...lancamentos){
+        lancamentos.forEach(l=>this.lancamentos.push(l)) 
+    }
+    sumario(){
+        let valorconsolidado=0
+        this.lancamentos.forEach(l=>{
+            valorconsolidado += l.valor
+        })
+        return valorconsolidado
+    }
+}
+const salario= new lancamento("salario" , 48634);
+const contadeluz= new lancamento("luz" , - 892);
+const conta = new cliclofinaceiro(6 , 2018);
+conta.addlancamento(salario , contadeluz)
+console.log(conta.sumario()) // 47742
