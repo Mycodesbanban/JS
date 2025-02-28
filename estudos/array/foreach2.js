33d28a60645760d0edb48dde7b6e09fe70597eff
maycon <mayconsrc1117@gmail.com>
2025-02-28 08:41:11 -0300

    up

---
+++ estudos/array/foreach2.js
@@ -0,0 +1,11 @@
+// forEach #02
+Array.prototype.forEach2 = function(callback){
+    for ( let i = 0 ; i < this.length; i++){
+        callback(this[i], i , this)
+    }
+}
+const aprovados= ["Junior" , "solidao" , "amor", "insegurança"];
+aprovados.forEach2(function(nome ,indice , array){ 
+   console.log(`${indice + 1}) ${nome}`);
+   console.log(array);
+})
