6b03d2720e82352d54b5e0075bbe535c690dbf08
maycon <mayconsrc1117@gmail.com>
2025-03-05 21:46:58 -0300

    Update
    Bot discord

---
+++ botdiscor/ticketinteraction.js
@@ -0,0 +1,8 @@
+const{modalBuilder , TextInputBuilder , ActionRowBuilder , TextInputStyle , ChannelType , EmbedBuilder , buttonBuilder , ButtonStyle , PermissionsbitField}=require("discord.js")
+const ticket =require("../Shemas/ticketShema");
+const { createTranscript}= require("discord-html-transcripts");
+
+modalBuilder.exeports={
+    name:"interactionCreate",
+    
+}
