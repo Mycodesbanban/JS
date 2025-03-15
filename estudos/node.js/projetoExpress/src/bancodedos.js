fb3be1f933a54069397ef9aa93496bae191fcae0
maycon <mayconsrc1117@gmail.com>
2025-03-15 20:46:38 -0300

    Update
    erros

---
+++ estudos/node.js/projetoExpress/src/bancodedos.js
@@ -0,0 +1,35 @@
+// Objeto que controla a sequência de IDs únicos para os produtos
+const sequencia = {
+    _id: 1,  // Variável privada que armazena o próximo ID disponível
+
+    get id() {  
+        return this._id++; // Retorna o ID atual e depois incrementa para o próximo
+    }
+};
+
+// Objeto que armazenará os produtos cadastrados
+const produtos = {};
+
+// Função para salvar um novo produto no "banco de dados" (objeto `produtos`)
+function salvarProduto(produto) {
+    if (!produto.id) produto.id = sequencia.id; // Se o produto não tiver um ID, gera um novo
+    produtos[produto.id] = produto; // Armazena o produto no objeto usando seu ID como chave
+    return produto; // Retorna o produto salvo
+}
+
+// Função para buscar um produto pelo ID
+function getProduto(id) {
+    return produtos[id] || {}; // Retorna o produto correspondente ou um objeto vazio se não existir
+}
+
+// Função para obter todos os produtos cadastrados
+function getProdutos() {
+    return Object.values(produtos); // Retorna um array contendo todos os produtos
+}
+
+// Exporta as funções para que possam ser usadas em outros arquivos
+module.exports = {
+    salvarProduto,  
+    getProduto,
+    getProdutos
+};
--- estudos/node.js/projetoExpress/src/servidor.js
+++ estudos/node.js/projetoExpress/src/servidor.js
@@ -2,6 +2,15 @@ const porta = 3003
 
 const express = require("express")
 const app = express()
+const bodyParse=require("body-parser");
+const importar=require("./bancodedos")
+
+ app.use(bodyParse.urlencoded({extended:true })) //.user é usado para registrar middlewares globais.
+//urlencoded() é um middleware do Express que permite processar dados enviados via formulários HTML, ele analisa dados da requisição
+ // extended efine como os dados serão interpretados.
+
+
+
 
 app.get("/produtos",(req , res , next)=>{
     console.log("middleware 1...")
@@ -10,12 +19,26 @@ app.get("/produtos",(req , res , next)=>{
 
 app.get("/produtos" , ( req , res , next)=>{
     // usamos a função middle 
-    res.send({nome:"notebook" , preco:37293 }) // send é um metodo do express.js que envia uma resposta HTTP para o cliente.
+        res.send(importar.getProdutos())
     // vai converte para o formato Json
 
 
 
 }) // get uma forma de requisição para web
+
+app.get("/produtos/:id" , (req , res)=>{
+    res.send(importar.getProdutos(req.params.id))
+})
+
+app.post('/produtos' , (req , res)=>{
+    const produto = importar.salvarproduto({
+        nome:req.body.nome,
+        preco:req.body.preco
+    })
+    res.send(produto) //JSON 
+    res.status(201).json(produto )
+
+})
 
 app.listen(porta , ()=>{ // chamos uma callback
     console.log(`Servidor rodando na porta ${porta}.`)
