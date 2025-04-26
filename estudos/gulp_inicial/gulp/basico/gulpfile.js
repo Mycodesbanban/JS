6f487b6c4c6ceb3e7bfe28d3c3f45d4fe182dc7e
maycon <mayconsrc1117@gmail.com>
2025-04-26 15:10:11 -0300

    Update

--- estudos/gulp_inicial/gulp/basico/gulpfile.js
+++ estudos/gulp_inicial/gulp/basico/gulpfile.js
@@ -1,2 +1,23 @@
-const gulp = require("gulp")
-const {series} = require("gulp")
+
+const gulp = require("gulp") 
+const {series , parallel}=require("gulp")
+//  parallel: Combina funções de tarefas e/ou operações compostas em operações maiores que serão executadas simultaneamente
+
+
+function copiar (callback){
+    // gulp.src(["pastaA/arquivo1.txt" , "pastaA/arquivo2.txt"]) // acessando os dois arquivos
+    gulp.src("pastaA/**/*.txt") // qualquer arquivo que esteja na pasta A e tenha a extensão .txt
+        .pipe(gulp.dest("pastaB"))
+    return callback() 
+
+    // src(): Cria um fluxo para ler objetos Vinyl do sistema de arquivos.Observação: BOMs (marcas de ordem de bytes) não têm utilidade em UTF-8 e serão removidos de arquivos UTF-8 lidos por src(), a menos que desativado usando a opção removeBOM / selecionar arquivos.
+
+    // pipe(): é usado para encadear tarefas no Gulp.Ou seja:Pega o resultado de uma etapa e passa para a próxima.
+    
+    // dest(): destino do arquivo/ para onde ele vai
+}
+
+
+module.exports.default= series(  
+    copiar, // depois o copiar 
+) 
