3c6d9ae4d9a289ca1f7f17729f2f591070c5d178
maycon <mayconsrc1117@gmail.com>
2025-05-01 14:16:33 -0300

    Update

--- estudos/gulp_inicial/gulp/spa/gulpTasks/app.js
+++ estudos/gulp_inicial/gulp/spa/gulpTasks/app.js
@@ -1,39 +1,43 @@
 const gulp = require("gulp") // importando o gulp
-const babel = require("gulp-babel")
-const {series , parallel} = require("gulp")
-const sass = require("gulp-sass")(require("sass"))
-const uglifycss = require("gulp-uglifycss")
-const concat = require("gulp-concat")
-const htmlmin = require("gulp-htmlmin")
-const uglify = require("gulp-uglify")
+const babel = require("gulp-babel") // importando o babel
+const {series , parallel} = require("gulp") // importando o series e o parallel
+const sass = require("gulp-sass")(require("sass")) // importando o sass 
+const uglifycss = require("gulp-uglifycss") // importando o uglifycss
+const concat = require("gulp-concat") // importando o concat
+const htmlmin = require("gulp-htmlmin") // importando o htmlmin
+const uglify = require("gulp-uglify") // importando o ugfify
 
 function AppHTML(){
-    return gulp.src("src/**/*.html")
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest("build"))
+    return gulp.src("src/**/*.html") // chamando todos os arquivos que tenha o .html
+        .pipe(htmlmin({collapseWhitespace: true})) // tira os espaçoes em branco como quebras de linhas 
+        .pipe(gulp.dest("build")) // destino 
 }
 
 function AppCSS(){
     return gulp.src("src/assets/sass/index.scss")
-    .pipe(sass().on("error" , sass.logError)) // pesquisar 
-    .pipe(uglifycss({"uglyComments":true})) 
-    .pipe(concat("app.min.css"))
-    .pipe(gulp.dest("build/assets/css"))
+    .pipe(sass().on("error" , sass.logError)) // tratar um erro caso houver 
+    .pipe(uglifycss({"uglyComments":true})) // minifica o css (remove os comentários )
+    .pipe(concat("app.min.css")) // junta todos os estilos em um só arquivo
+    .pipe(gulp.dest("build/assets/css")) //destino
 }
 
 function AppJS(){
     return gulp.src("src/assets/js/**/*.js")
-        .pipe(babel({presets:["env"]})) // pesquisar 
-        .pipe(uglify()) // pesquisar 
-        .pipe(concat("app.min.js")) // pesquisar
-        .pipe(gulp.dest("build/assets/js"))
+        .pipe(babel({presets:["env"]})) // coloca para uma versão compativel (ex:Es5) 
+        .pipe(uglify()) // minifica os arquivos js (remove espaçoe e renomeia variaveis)
+        .pipe(concat("app.min.js")) // Junta tudo em um unico arquivo
+        .pipe(gulp.dest("build/assets/js")) // destino
 }
 
 function AppIMG(){
-    return gulp.src("src/assets/imgs/**/*.*")
-    .pipe(gulp.dest("build/assets/img"))
+    return gulp.src("src/assets/imgs/**/*.*") // pega todos os arquivos
+    .pipe(gulp.dest("build/assets/img")) //destino 
 }
 
+gulp.task("appHTML" , AppHTML) // define a função appHTML 
+gulp.task("appIMG" , AppIMG) // define a função appIMG 
+gulp.task("appJS" , AppJS) // define a função appJS
+gulp.task("appCSS", AppCSS) // define a função appCSS
 
 module.exports = { 
     AppHTML , 
@@ -42,9 +46,14 @@ module.exports = {
     AppIMG 
 }
 
-// htmlmin:
+// htmlmin: Um minificador HTML simples 
 
-//sass:
+//sass:  é uma linguagem de extensão do CSS que permite escrever estilos de forma mais eficiente, organizada e reutilizável.
 
-//collapseWhitespace:
+//collapseWhitespace: Remove espaços em branco desnecessários no HTML (como quebras de linha, tabs, espaços extras), resultando em HTML mais compacto.
 
+// uglify: é um plugin do Gulp que minifica arquivos JS. Ele remove espaços, comentários e reduz nomes de variáveis/funções para deixar os arquivos menores e mais rápidos para carregar no navegador.
+
+// babel:  é uma ferramenta de compilação de código fonte que permite a conversão de código moderno para versões compatíveis com navegadores mais antigos
+
+// babel(presents) : Presets são conjuntos de plugins Babel que transformam sintaxes específicas do JS eles facilitam a configuração porque evitam que você precise listar plugin por plugin
--- estudos/gulp_inicial/gulp/spa/gulpTasks/servidor.js
+++ estudos/gulp_inicial/gulp/spa/gulpTasks/servidor.js
@@ -1,12 +1,24 @@
 const gulp = require("gulp")
+const webserve = require("gulp-webserver") 
+const watch = require ("gulp-watch")
 
-function monitorarArquivos(callback){
-    return callback()
 
+function servidor (){
+    return gulp.src("build")
+        .pipe(webserve({
+            port:8080 ,  // porta do serve 
+            open:true ,  // se quer que abrimos o browser/navegador
+            livereload: true, // reniciar se automaticamente 
+        }))
+    
 }
-
-function servidor ( callback){
-    return callback()
+function monitorarArquivos(callback){
+    watch("src/**/*.html" , () => gulp.series("appHTML")()) // se o algum .html for alterado executa a tarefa appHTML 
+    watch("src/**/*.js" , () => gulp.series("appJS")()) // se algum .js for alterado executa a tarefa executa o appJS 
+    watch("src/**/*.scss" , () => gulp.series("appCSS")()) // se algum .scss for alterado executa a tarefa appCSS
+    watch("src/assets/imgs/**/*.*" , () => gulp.series("appIMG")()) // se qualquer arquivo dentro de imgs for alterado executa o appIMG
+    
+    return callback() // falando para o gulp que a tarefa acabou 
 
 }
 
@@ -14,3 +26,17 @@ module.exports = {
     monitorarArquivos , 
     servidor , 
 }
+
+
+// webserver (gulp) : iniciar um servidor local com suporte a LiveReload, navegação automática e fallback para aplicações SPA
+
+// open (gulp-webserver) : abre o servidor pelo o navegador 
+
+// port (gulp-webserver) : o numero da porta que vamos escolher  
+
+// livereload (gulp-webserver) : faz um reload na pagina se alguma coisa for mudada
+
+// watch (gulp-watch) : Permite observar globos e executar uma tarefa quando uma alteração ocorre. As tarefas são tratadas uniformemente com o restante do sistema de tarefas/Quando você modifica um arquivo, ele executa automaticamente uma tarefa que você escolher.
+
+// gulp.task():Define uma tarefa dentro do sistema de tarefas. A tarefa pode então ser acessada a partir da linha de comando e das APIs series(), parallel() e lastRun() / como dar um nome para um bloco de código que faz alguma coisa, para você poder chamar e reutilizar depois.
+
