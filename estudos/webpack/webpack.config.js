e9303c0b09c01f4e256bef736285f486c8c7996d
maycon <mayconsrc1117@gmail.com>
2025-05-03 22:16:10 -0300

    Update

--- estudos/webpack/webpack.config.js
+++ estudos/webpack/webpack.config.js
@@ -1,10 +1,25 @@
 const webpack = require("webpack")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     mode:"development",
     entry:"./src/principal.js",
     output:{
         filename:"principal.js",
-        path:__dirname+"/public"
+        path: __dirname+ "/public"
+    } , 
+    Plugins:[
+        new MiniCssExtractPlugin ({
+            
+        })
+    ],
+    module:{ // pesquisar 
+        rules:[{ // rules são as regras 
+            test:/\.css$/, // me explique isso 
+            use:[ // me exlique isso 
+                "style-loader" ,  // Adiciona CSS a dom injentando a tag </style>
+                "css-loader", //interpreta @import , url()...
+            ]
+        }]
     }
 }
