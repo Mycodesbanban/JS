d1a52e39239e3fb0ce2300302bb29ddb14ddebcd
maycon <mayconsrc1117@gmail.com>
2025-05-15 15:56:22 -0300

    update

---
+++ estudos/react/ReactCSS/cssreact/src/components/Mycomponent.jsx
@@ -0,0 +1,12 @@
+import "./Mycomponent.css"
+
+const Mycomponent = () => {
+  return (
+    <div>
+        <h1>Mycomponent</h1>
+        <p>paragrafos</p>
+    </div>
+  )
+}
+
+export default Mycomponent
