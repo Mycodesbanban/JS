6ce4fc363f651cd49a62baceceabb2ae99154b3f
maycon <mayconsrc1117@gmail.com>
2025-05-18 11:50:58 -0300

    update

--- estudos/react/formularios/src/components/CreateForm.jsx
+++ estudos/react/formularios/src/components/CreateForm.jsx
@@ -8,6 +8,10 @@ const CreateForm = () => {
                 <input type="text" name="name" placeholder="digite seu nome"/> 
                 {/* criando um campo de texto que vai receber o label que decreve o que é para fazer nesse campo , e temos o htmlFor  , ele é igual o for so que estamos usando react então temos que usar o htmlFor*/}
             </div>
+            <label >
+              <span>email</span>
+              <input type="email" name="email" placeholder="digite seu email" />
+            </label>
             <input type="submit" value="enviar" /> 
             {/* criando um botão para enviar os dados */}
         </form>
