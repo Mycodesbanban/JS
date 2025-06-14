f871773299b031cae8a600d0f898a02d75bc8fbd
maycon <mayconsrc1117@gmail.com>
2025-06-13 21:45:09 -0300

    Update

---
+++ estudos/react/react-router/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>hello word</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+// SPA/Single Page Applicationss:aplicações web que interagem com o usuário atualizando dinamicamente a página atual, ao invés de carregar novas páginas inteiras do servidor
