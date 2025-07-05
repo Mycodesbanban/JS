b95b705cfc62490dd1d993412a28022ad11dab53
Maycon Santos Rocha Costa <mayconsrc1117@gmail.com>
2025-07-05 15:30:10 -0300

    Update

--- estudos/node.js/express/server.js
+++ estudos/node.js/express/server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const app = express()
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
 
-app.get("/" , (req , res)=>{
-    res.send("hello <b>word</b> gg game funcionou caralhoooooooooooooooooooo!")
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
 });
-app.get("/contato" , (req , res)=>{
-    res.send("Obrigado por entrar em contato com a gente")
-})
-app.listen(3000 , ()=>{
-    console.log("'Servidor executando na porta 3000")
-}); 
 
-
-//* req.params:Usado para parâmetros de rota ou seja, dados inseridos diretamente na URL
-
-//* req.query:Usado para query strings na URL (após o ?). Muito comum em filtros, paginações, etc
-
-//* req.body: Usado para acessar o corpo da requisição,
