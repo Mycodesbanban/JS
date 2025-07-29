68499928a3e8075e75fed2607d84e37197a76f0d
Maycon Santos Rocha Costa <mayconsrc1117@gmail.com>
2025-07-29 15:56:47 -0300

    update

--- estudos/node.js/express/server.js
+++ estudos/node.js/express/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+
+const mongoose = require("mongoose"); // unsando o pacote mongoose
+
+const connectString= 'mongodb+srv://MayconBancoMongodb:maycon1716@cluster0.2jfkzls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' // colocando a conexão do servidor
+
+mongoose.connect(connectString, {useNewUrlParser:true , useUnifiedTopology:true})//conectando o servidor
+  .then(()=> {
+    app.emit() // emite eventos/dados
+  })
+  
+
+
 const routes = require('./routes');
 const path = require('path');
 
